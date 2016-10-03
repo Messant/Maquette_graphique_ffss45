12e9f13badf23d87dc4cbbf43dad56bceac3f6a7
Jasdero <houssemaine.j@gmail.com>
2016-10-03 15:18:12 +0200

    ajustement_responsive

--- presentation.php
+++ presentation.php
@@ -4,7 +4,7 @@
 
         <!--Texte à gauche de la fenêtre-->
 
-        <div class="col-lg-7 col-md-7 hidden-xs">
+        <div class="col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-1 col-sm-5 hidden-xs">
             <h3>Qui sommes-nous ?</h3>
             <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
             <h3>Nos métiers</h3>
@@ -16,7 +16,7 @@
         </div>
 
         <!--Image de la partie droite-->
-        <div class="col-lg-5 col-md-5 hidden-xs">
+        <div class="col-lg-offset-1 col-lg-4 col-md-offset-1 col-md-5 col-sm-offset-1 col-sm-5 hidden-xs">
             <img src="Images/Affiche Salon des Maires-01.png" alt="" class="img-responsive">
         </div>
     </div>
