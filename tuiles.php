539b9bbaedd4447e170154b9943216013aa39534
Messant <messant.d@gmail.com>
2016-10-03 18:00:18 +0200

    intégration tuiles

---
+++ tuiles.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 03/10/16
+ * Time: 15:09
+ */
+?>
+
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-md-6 col-lg-4">
+                <button type="button" class="btn btn-block">Espace Particuliers</button>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-md-6 col-lg-4">
+                <button type="button" class="btn btn-block">Espace Professionnels</button>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-md-6 col-lg-4">
+                <button type="button" class="btn btn-block">Evènements</button>
+            </div>
+        </div>
+    </div>
+</body>
