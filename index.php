920c74697aff999cc4f5d37d1eec70eec87be854
Jasdero <houssemaine.j@gmail.com>
2016-10-03 14:27:20 +0200

    changement_php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+    include 'head.php';
+    include 'header.php';
+    include 'presentation.php';
+
+                    </div>
+                </div>
+
+        </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
