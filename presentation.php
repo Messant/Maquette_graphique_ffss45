9749f2a950fdcbecccc281c18547d887558cd177
Jasdero <houssemaine.j@gmail.com>
2016-10-03 14:33:52 +0200

    squelette_presentation

---
+++ presentation.php
@@ -0,0 +1,24 @@
+
+<div class="container-fluid">
+    <div class="row">
+
+        <!--Texte à gauche de la fenêtre-->
+
+        <div class="col-lg-7 col-md-7 hidden-xs">
+            <h3>Qui sommes-nous ?</h3>
+            <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
+            <h3>Nos métiers</h3>
+            <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
+            <h3>Nos missions</h3>
+            <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
+            <a href="#" class="btn btn-primary btn-lg active" role="button">En savoir plus</a>
+
+        </div>
+
+        <!--Image de la partie droite-->
+        <div class="col-lg-5 col-md-5 hidden-xs">
+            <img src="Images/Affiche Salon des Maires-01.png" alt="" class="img-responsive">
+        </div>
+    </div>
+</div>
+
