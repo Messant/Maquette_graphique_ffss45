b5c5bad4372c4dd27509b9e25b101c2ae7c47514
Jasdero <houssemaine.j@gmail.com>
2016-10-04 15:35:44 +0200

    ajout_formulaire

--- formulaire.php
+++ formulaire.php
@@ -2,6 +2,25 @@
     <div class="row">
         <!--formulaire-->
         <div class="col-lg-5">
+            <h4>Contactez-nous</h4>
+            <form>
+
+
+                <div class="form-group">
+                    <label for="nom">Nom</label>
+                    <input type="text" class="form-control" id="nom" placeholder="Nom">
+                </div>
+                <div class="form-group">
+                    <label for="prenom">Prénom</label>
+                    <input type="text" class="form-control" id="prenom" placeholder="Prénom">
+                </div>
+                <div class="form-group">
+                    <label for="Email">E-mail</label>
+                    <input type="email" class="form-control" id="Email" placeholder="Email">
+                </div>
+                <textarea class="form-control" rows="3" placeholder="Quelle est votre demande ?"></textarea><br>
+                <button type="submit" class="btn btn-default center-block">Submit</button>
+            </form>
 
         </div>
 
@@ -12,7 +31,7 @@
         </div>
         <!--Map-->
         <div class="col-lg-5">
-            
+
         </div>
 
 
