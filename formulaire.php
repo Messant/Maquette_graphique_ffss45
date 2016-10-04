d574ad3096506f4eea5d5334013f8f499ca4d6fb
Jasdero <houssemaine.j@gmail.com>
2016-10-04 15:23:38 +0200

    creation_squelette

---
+++ formulaire.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+    <div class="row">
+        <!--formulaire-->
+        <div class="col-lg-5">
+
+        </div>
+
+        <!--infos contact-->
+        <div class="col-lg-2">
+
+
+        </div>
+        <!--Map-->
+        <div class="col-lg-5">
+            
+        </div>
+
+
+    </div>
+
+</div>
