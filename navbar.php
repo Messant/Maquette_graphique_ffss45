890ffab5995a93a87f281bc3ffd9b4021e347b2a
Messant <messant.d@gmail.com>
2016-10-04 14:43:13 +0200

    intégration navbar

---
+++ navbar.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 04/10/16
+ * Time: 10:12
+ *
+ */
+?>
+<nav class="navbar navbar-default navbar-fixed">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse js-navbar-collapse">
+            <ul class="nav nav-justified ">
+                <li class="dropdown dropdown-large">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Particuliers <b class="caret"></b></a>
+                    <ul class="dropdown-menu dropdown-menu-large row">
+                        <li class="dropdown-header">POUR DEBUTER</li>
+                        <li><a href="#">IPS</a></li>
+                        <li><a href="#">PSC1</a></li>
+                        <li class="dropdown-header">SECOURS SPORTIF</li>
+                        <li><a href="#">PSS1</a></li>
+                        <li class="dropdown-header">INCENDIE</li>
+                        <li><a href="#">SSIAP</a></li>
+                        <li class="dropdown-header">SECOURS EN EQUIPE</li>
+                        <li><a href="#">PSE1</a></li>
+                        <li><a href="#">PSE2</a></li>
+                        <li><a href="#">FCPSE1</a></li>
+                        <li><a href="#">FCPSE2</a></li>
+                        <li><a href="#">Chef d'équipe</a></li>
+                        <li><a href="#">Chef de poste</a></li>
+                        <li class="dropdown-header">FORMATEURS</li>
+                        <li><a href="#">Formateur PSC</a></li>
+                        <li><a href="#">Formateur PS</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown dropdown-large">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Professionnels <b class="caret"></b></a>
+                    <ul class="dropdown-menu dropdown-menu-large row">
+                        <li class="col-sm-3">
+                            <ul>
+                                <li class="dropdown-header">Glyphicons</li>
+                                <li><a href="#">Available glyphs</a></li>
+                                <li class="disabled"><a href="#">How to use</a></li>
+                                <li><a href="#">Examples</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Dropdowns</li>
+                                <li><a href="#">Example</a></li>
+                                <li><a href="#">Aligninment options</a></li>
+                                <li><a href="#">Headers</a></li>
+                                <li><a href="#">Disabled menu items</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown dropdown-large">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Evènements <b class="caret"></b></a>
+                    <ul class="dropdown-menu dropdown-menu-large row">
+                        <li class="col-sm-3">
+                            <ul>
+                                <li class="dropdown-header">Glyphicons</li>
+                                <li><a href="#">Available glyphs</a></li>
+                                <li class="disabled"><a href="#">How to use</a></li>
+                                <li><a href="#">Examples</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Dropdowns</li>
+                                <li><a href="#">Example</a></li>
+                                <li><a href="#">Aligninment options</a></li>
+                                <li><a href="#">Headers</a></li>
+                                <li><a href="#">Disabled menu items</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Rejoignez-nous</a></li>
+        </div><!-- /.nav-collapse -->
+    </nav>
