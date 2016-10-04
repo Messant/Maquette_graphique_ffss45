ffe7ca8043d5b8683d61fd4e9fae91b1d5009919
Jasdero <houssemaine.j@gmail.com>
2016-10-04 17:42:13 +0200

    reglage_responsive

--- formulaire.php
+++ formulaire.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="row">
         <!--formulaire-->
-        <div class="col-lg-offset-1 col-lg-4">
+        <div class="col-lg-offset-1 col-lg-3 col-md-5 col-sm-offset-1 col-sm-4 col-xs-12">
             <h4>Contactez-nous</h4>
             <form>
 
@@ -25,22 +25,25 @@
         </div>
 
         <!--infos contact-->
-        <div class="col-lg-2">
+        <div class="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-5 col-sm-offset-3 col-sm-3 col-xs-offset-4 col-xs-8">
             <strong>FFSS 45 - Secouristes du Loiret</strong><br>
                 <ul>
                 <li>15 Quai du Roi</li>
                 <li>45000 Orléans</li>
                 <li>Tél : 02.38.43.12.73</li>
                 </ul>
-            <a href="">Plan du site</a>
 
 
         </div>
+        <div class="hidden-lg hidden-md hidden-sm col-xs-offset-5 col-xs-7">
+            <a href="http://www.openstreetmap.org/?mlat=47.89969&mlon=1.92325#map=19/47.89969/1.92325">Voir sur la carte</a>
+        </div>
         <!--Map-->
-        <div class="col-lg-offset-1 col-lg-2">
+          <div class="col-lg-offset-1 col-lg-3 col-md-offset-4 col-md-8 col-sm-offset-4 col-sm-8 hidden-xs">
             <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=1.9215559959411623%2C47.89889823611775%2C1.9249221682548525%2C47.900513050739406&amp;layer=mapnik&amp;marker=47.89970564972463%2C1.9232404232025146" style="border: 1px solid black"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=47.89971&amp;mlon=1.92324#map=19/47.89971/1.92324">View Larger Map</a></small>
 
         </div>
+
 
 
     </div>
