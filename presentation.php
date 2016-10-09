6e94cf3366beffee2285d59cf15df68aac3a3673
Jasdero <houssemaine.j@gmail.com>
2016-10-09 08:20:20 +0200

    centrage text pres

--- presentation.php
+++ presentation.php
@@ -3,18 +3,18 @@
     <div class="row">
 
         <!--Texte à gauche de la fenêtre-->
+        <div class="pres_text">
+            <div class="col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-1 col-sm-5 hidden-xs">
+                <h3>Qui sommes-nous ?</h3>
+                <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
+                <h3>Nos métiers</h3>
+                <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
+                <h3>Nos missions</h3>
+                <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
+                <a href="#" class="btn btn-primary btn-lg active" role="button">En savoir plus</a>
 
-        <div class="col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-1 col-sm-5 hidden-xs">
-            <h3>Qui sommes-nous ?</h3>
-            <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
-            <h3>Nos métiers</h3>
-            <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
-            <h3>Nos missions</h3>
-            <p>Sed et est quibusdam quaerat laborum quia amet tempore. Enim ex quas velit architecto hic. Ad maiores officia totam eaque quibusdam voluptas omnis corrupti. Omnis labore maiores est odit. Ratione quia dolores vel.</p>
-            <a href="#" class="btn btn-primary btn-lg active" role="button">En savoir plus</a>
-
+            </div>
         </div>
-
         <!--Image de la partie droite-->
         <div class="col-lg-offset-1 col-lg-4 col-md-offset-1 col-md-5 col-sm-offset-1 col-sm-5 hidden-xs">
             <img src="Images/AfficheSalondes Maires-01.png" alt="" class="img-responsive">
